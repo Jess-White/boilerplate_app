d826710a7be4cf10953ef742e931b3ec71bae546
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-05-23 19:04:32 -0500

    login changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
       begin
         decoded_token = JWT.decode(
           token,
-          Rails.application.credentials.fetch(:secret_key_base),
+          Rails.application.credentials[Rails.env.to_sym].fetch(:secret_key_base),
           true,
           { algorithm: "HS256" }
         )
